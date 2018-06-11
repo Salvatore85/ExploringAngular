0fc549afa7ebb72ce3062e092c8a75e79e1fe291
unknown <salvatore.m@gmail.com>
2018-06-11 16:01:44 +0200

    Playing with databinding

--- TaskManager/src/app/app.component.ts
+++ TaskManager/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
+import { Priority } from './priority'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Task Manager</h1>
+              <ul>
+    <li *ngFor="let task of tasks" [class.low-priority]="task.priority===low" [class.high-priority]="task.priority===high">
+      {{task.description}}
+    </li>
+  </ul>`,
+  styles: [`
+  .high-priority {color: rgb(176, 50, 0);}
+  .low-priority {color: rgb(0, 153, 176);}
+  `],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  low = Priority.Low;
+  high = Priority.High;
+
+  tasks: Task[];
+  constructor() {
+    this.tasks = [
+      { id: 1, description: 'fix heisenburg', completed: false, priority: Priority.Normal},
+      { id: 1, description: 'set up fear-driven development', completed: false, priority: Priority.High},
+      { id: 1, description: 'fix hindenburg', completed: false, priority: Priority.Low}
+    ]
+  }
 }
---
+++ TaskManager/src/app/task.ts
@@ -0,0 +1,8 @@
+import { Priority } from "./priority";
+
+export interface Task {
+    id: number;
+    description: string;
+    completed: boolean;
+    priority: Priority;
+}
