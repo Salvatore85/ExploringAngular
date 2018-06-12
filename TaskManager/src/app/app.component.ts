5784c3ccdf37c99cfd6d982843a70d385c384cab
unknown <salvatore.m@gmail.com>
2018-06-12 08:56:23 +0200

    Updated Data Binding test

--- TaskManager/src/app/app.component.ts
+++ TaskManager/src/app/app.component.ts
@@ -4,9 +4,19 @@ import { Priority } from './priority'
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Task Manager</h1>
-              <ul>
-    <li *ngFor="let task of tasks" [class.low-priority]="task.priority===low" [class.high-priority]="task.priority===high">
+  template: `
+  <h1>Task Manager</h1>
+  <div>
+    <input [(ngModel)]="newTask.description"/>
+    <select [(ngModel)]="newTask.priority">
+      <option *ngFor="let priority of priorities" [ngValue]="priority">{{getNameForPriority(priority)}}</option>
+    </select>
+    <a href="" (click)="addTask(newTask, $event)">add new</a>
+  </div>
+  <ul>
+    <li *ngFor="let task of tasks" 
+    [class.low-priority]="task.priority===low" 
+    [class.high-priority]="task.priority===high">
       {{task.description}}
     </li>
   </ul>`,
@@ -14,12 +24,30 @@ import { Priority } from './priority'
   .high-priority {color: rgb(176, 50, 0);}
   .low-priority {color: rgb(0, 153, 176);}
   `],
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
   low = Priority.Low;
   high = Priority.High;
+  
+  newTask: Task = { id: 0, description: '', completed: false, priority: Priority.Normal };
+  priorities = [Priority.Low, Priority.Normal, Priority.High];
+
+  getNameForPriority(priority: Priority): string {
+    return Priority[priority];
+  }
+
+  getNextId(): number {
+    return this.tasks.length + 1;
+  }
+
+  addTask(task: Task): void {
+    task.id = this.getNextId();
+    this.tasks.push(task);
+    this.newTask = { id: 0, description: '', completed: false, priority: Priority.Normal };
+    event.preventDefault();
+  }
 
   tasks: Task[];
   constructor() {
