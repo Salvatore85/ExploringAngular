4f85bc906b63ade8012bd1a908863dea53b162fc
unknown <salvatore.m@gmail.com>
2018-06-12 15:39:48 +0200

    Updated custom temperature pipe

--- TaskManager/src/app/temperature.pipe.ts
+++ TaskManager/src/app/temperature.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: number, type: string, places: number): string {
+    //set defaults
+    type = type || 'kelvin';
+    places = places || 2;
+    let temp: number;
+
+    switch (type) {
+      case 'celsius':
+        temp = value - 273.15;
+        return temp.toFixed(places) + "\u00B0C";
+      case 'fahrenheit':
+        temp = (value * 9 / 5) - 459.67;
+        return temp.toFixed(places) + "\u00B0F";
+      default:
+        return value.toFixed(places) + "K";
+    }
   }
 
 }
