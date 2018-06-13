03ad330d1d7054abf803a81fcd828e5d17147578
unknown <salvatore.m@gmail.com>
2018-06-13 15:34:31 +0200

    Minor changes

--- RabbitShelter/src/app/app.module.ts
+++ RabbitShelter/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { AppRoutingModule } from './app.routes';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import { ContactComponent } from './contact/contact.component';
