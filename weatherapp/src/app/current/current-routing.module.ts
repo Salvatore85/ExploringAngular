0225b981ff4b16756c707e53f847f4fb4a38d8fc
unknown <salvatore.m@gmail.com>
2018-06-15 12:02:08 +0200

    Updated weather app with routing

--- weatherapp/src/app/current/current-routing.module.ts
+++ weatherapp/src/app/current/current-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CurrentComponent } from './current/current.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'current', component: CurrentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- weatherapp/src/app/history/history-routing.module.ts
+++ weatherapp/src/app/history/history-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HistoryModule } from './history.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: 'history', component: HistoryModule,
+    children: [
+      { path: }
+    ] 
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
